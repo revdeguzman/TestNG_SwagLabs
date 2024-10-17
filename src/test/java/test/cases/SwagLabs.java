b63ec7a65f9588cdf3eba5cf5e057b8b221b60ff
rtdeguzman <rtdeguzman@ACSS-LTP151.domain.aeoncredit.com.ph>
2024-10-17 16:17:09 +0800

    updated version

--- src/test/java/test/cases/SwagLabs.java
+++ src/test/java/test/cases/SwagLabs.java
@@ -1,8 +1,11 @@
 package test.cases;
 
 import java.time.Duration;
+
+import org.apache.commons.collections4.Get;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -36,6 +39,40 @@ public class SwagLabs extends BaseFiles {
 			
 			//Print
 			System.out.println("Executed: Test Case 1: Sauce Labs Fleece Jacket");
+			
+		  }
+	      
+	      @Test(dataProvider = "testData")
+		  public void TC002(String product) throws InterruptedException {
+			
+			//Add Item to Cart
+			driver.findElement(By.id(product)).click();
+			
+	    	WebElement num_cart = driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span"));
+			int count_cart = Integer.parseUnsignedInt(num_cart.getText());
+			
+			if (count_cart >= 6) {
+				
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+				driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+				
+				//Click Checkout
+				driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+				
+				driver.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys("Test First Name");
+				driver.findElement(By.xpath("//*[@id=\"last-name\"]")).sendKeys("Test Last Name");
+				driver.findElement(By.xpath("//*[@id=\"postal-code\"]")).sendKeys("Test Zip Code");
+				driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+				
+				//Click Finish
+				driver.findElement(By.cssSelector("button[id='finish']")).click();
+				
+				//Click Home
+				driver.findElement(By.xpath("//*[@id=\"back-to-products\"]")).click();
+				
+				//Print
+				System.out.println("Executed: Test Case 1: Sauce Labs Fleece Jacket");
+			}
 			
 		  }
 	      
